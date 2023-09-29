b7399efcb6205960baa09e4c56e075c30999456d
null <null>
2023-09-29 11:44:06 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multidimensionales_G2_2024_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,,] miArreglo = { { { 'a', 'b','c' }, { 'd', 'e', 'f' } }, { { 'g', 'h', 'i' }, { 'j', 'k', 'l' } } };
+
+            Console.WriteLine(miArreglo[1, 1, 1]);
+            Console.WriteLine( miArreglo.Length);
+            Console.ReadLine();
+            
+        }
+    }
+}
